8d6587e63da8991254c87b5eedcb4fb68a2abdf4
luischaparro <luisesneidher-1993@hotmail.com>
2024-05-22 17:00:28 -0500

    responsive-textShadow

--- src/Screen/Inicio.jsx
+++ src/Screen/Inicio.jsx
@@ -3,9 +3,13 @@ import logo from "./Assets/logo.png"
 
 const Inicio = () => {
   return (
-    <div className=''>
-      <main className="bg-[url('/src/Screen/Assets/fondo_inicio.jpg')] flex w-full h-screen justify-around items-center">
-        <p className="w-1/4 h-1/4 text-[#f8f4e1] font-extrabold">
+    <div>
+      <main className="bg-center bg-no-repeat bg-cover bg-[url('/src/Screen/Assets/fondo_inicio.jpg')] 
+      w-full h-screen items-center flex flex-col justify-evenly md:flex-row md:justify-around
+      ">
+        <p className="w-3/4 text-[#f8f4e1] font-extrabold
+        md:w-1/4 md:h-1/4
+        drop-shadow-[0_2px_3px_rgba(0,0,0,0.9)]">
           El Centro de Historia de Pinchote Álvaro Sarmiento Santander es un
           lugar ideal para aprender sobre la rica historia y cultura del
           municipio. Es una visita obligada para cualquier persona que esté
@@ -13,7 +17,8 @@ const Inicio = () => {
         </p>
 
         <img
-          className="w-1/4 h-2/4 rounded-full"
+          className="w-[200px] h-[200px] rounded-full
+          md:w-[300px] md:h-[300px]"
           src={logo}
           alt="Logo del centro de historia de Pinchote."
         />
@@ -21,7 +26,9 @@ const Inicio = () => {
 
       <section className="bg-center bg-no-repeat bg-cover bg-fixed w-full h-screen flex 
       justify-around items-center bg-[url('/src/Screen/Assets/fondo2.jpg')]">
-        <p className="w-1/4 h-1/4 text-[#f8f4e1] font-extrabold">
+        <p className="font-extrabold w-3/4 text-[#f8f4e1] font-extrabold
+        md:w-1/4
+        drop-shadow-[0_2px_3px_rgba(0,0,0,0.9)]">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
           ullam aliquid ad. Harum dignissimos laudantium, officiis delectus ea
           numquam rem fugiat, suscipit nihil ipsa debitis, tenetur voluptate
