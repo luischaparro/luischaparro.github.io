ece1e2000dc11043278b90a575674d55c508c99a
roller <43080946+allxdxd@users.noreply.github.com>
2024-06-09 18:15:23 -0500

    Changes: 	renamed:    robots.txt -> public/robots.txt 	renamed:    sitemap.xml -> public/sitemap.xml 	modified:   src/Screen/Blog.jsx

--- src/Screen/Blog.jsx
+++ src/Screen/Blog.jsx
@@ -1,25 +1,43 @@
-import Blogs from "../components/Blogs.jsx"
-import Container from "../components/Container.jsx"
-import datos from "../Assets/datos.json"
+import Blogs from "../components/Blogs.jsx";
+import Container from "../components/Container.jsx";
+import datos from "../Assets/datos.json";
 // imagen temporal
-import img from "../Assets/img.js"
-const imgSrc = `data:image/jpeg;base64,${img}`
+import img from "../Assets/img.js";
+import { useEffect, useState } from "react";
+
+const api = "https://chpapi.vercel.app/blog";
+const imgSrc = `data:image/jpeg;base64,${img}`;
 
 const Blog = () => {
+  // Estado que guarde los blogs
+  let [data, setData] = useState([]);
+  // Fecth a la API de los blog
+  useEffect(() => {
+    console.log("Haciendo fetch");
+    fetch(api, { headers: { "Content-Type": "application/json" } })
+      .then((res) => res.json())
+      .then((res) => {
+        // Cambiando el estado
+        console.log(res);
+        setData(res);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <Container>
-        {datos.map( dato => 
+        {data.map((dato) => (
           <Blogs
-            imagen={imgSrc}
-            titulo={dato.titulo}
-            fecha={dato.fecha}
-            contenido={dato.contenido}
+            imagen={`data:image/jpeg;base64,${dato.img}`}
+            titulo={dato.title}
+            fecha={dato.date}
+            contenido={dato.body}
           />
-        )}
+        ))}
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
